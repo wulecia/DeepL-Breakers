2a5fd299d6e76699df1c4242d3f8a6368d0bd277
wulecia <114996403+wulecia@users.noreply.github.com>
2025-05-16 18:10:56 +0200

    Update hasoc_model.py

--- coline/hasoc_model.py
+++ coline/hasoc_model.py
@@ -169,7 +169,7 @@ def train_model(task, model_wrapper, dataset, tokenizer, resume=True):
         per_device_eval_batch_size=16,
         save_strategy="steps" if task == "B" else "epoch",
         save_steps=500 if task == "B" else None,
-        evaluation_strategy="epoch",
+        eval_strategy="epoch",
         eval_steps=500 if task == "B" else None,
         logging_steps=100,
         learning_rate=2e-5,
