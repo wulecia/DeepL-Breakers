09df865ab24d88e23bdb1e738965e88f2a0e4546
Biocchi Paola <biocchi@haas001.ds-a4-r02.cct.rcp.epfl.ch>
2025-05-23 15:11:17 +0200

    modifs

--- coline/coline_features.py
+++ coline/coline_features.py
@@ -58,7 +58,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_paola = Paola()
 
 # Load weights BEFORE wrapping in DataParallel
-state_dict = torch.load("../paola/results/best_model_2025-05-21_14-23-53.pth", map_location=device, weights_only=True)
+state_dict = torch.load("../paola/best_model.pth", map_location=device, weights_only=True)
 model_paola.load_state_dict(state_dict)
 
 # Then wrap in DataParallel if multiple GPUs
