8b13533c684311b71b50ac24d9ac040b1b40ee99
Coline Leteurtre <coline.leteurtre@epfl.ch>
2025-05-26 15:20:09 +0000

    files added

--- hasoc_model_boosted/show_cm.py
+++ hasoc_model_boosted/show_cm.py
@@ -4,56 +4,14 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 
-experiment = "load_freeze_grid"
+experiment = "load_grid"
 
 summary = pd.read_csv(f"./results/{experiment}/grid_metrics/summary_f1_accuracy.csv") 
 confmat_dir = Path(f"./results/{experiment}/grid_metrics")
 save_dir = Path(f"./results/{experiment}/grid_metrics_final")
 save_dir.mkdir(parents=True, exist_ok=True)
 
-'''
-top_models = summary.groupby("task").apply(lambda g: g.nlargest(8, "f1_weighted")).reset_index(drop=True)
-
-for task in top_models["task"].unique():
-    task_models = top_models[top_models["task"] == task]
-    
-    fig, axes = plt.subplots(2, 4, figsize=(20, 10))
-    axes = axes.flatten()
-    
-    for idx, (_, row) in enumerate(task_models.iterrows()):
-        model_id = row["model_id"]
-        confmat_path = confmat_dir / f"confmat_{model_id}.csv"
-        
-        ax = axes[idx]
-        if confmat_path.exists():
-            confmat = pd.read_csv(confmat_path, header=None, skiprows=1).values
-            sns.heatmap(confmat, annot=True, fmt="d", cmap="Blues", ax=ax, cbar=False)
-            
-            title_line1 = model_id
-            title_line2 = (
-                f"Weighted F1: {row['f1_weighted']:.3f} | "
-                f"Macro F1: {row['f1_macro']:.3f} | "
-                f"Acc: {row['accuracy']:.3f}"
-            )
-            ax.set_title(f"{title_line1}\n{title_line2}", fontsize=10, pad=12)
-        else:
-            ax.set_title(f"{model_id}\nMissing File", fontsize=10)
-            ax.axis("off")
-        
-        ax.set_xlabel("Predicted")
-        ax.set_ylabel("True")
-    
-    fig.suptitle(f"Top 8 Models for Task {task}", fontsize=18, y=1.05)
-    plt.tight_layout(rect=[0, 0, 1, 0.95])
-
-    save_path = save_dir / f"Top_8_Models_Task_{task}.png"
-    fig.savefig(save_path, dpi=300, bbox_inches="tight")
-    plt.close(fig)
-
-    print(f"Saved: {save_path}")
-'''
-# Only get top 1 model per task
-top_models = summary.groupby("task").apply(lambda g: g.nlargest(1, "f1_weighted")).reset_index(drop=True)
+top_models = summary.groupby("task").apply(lambda g: g.nlargest(1, "f1_macro")).reset_index(drop=True) #1 fot Top 1
 
 for task in top_models["task"].unique():
     row = top_models[top_models["task"] == task].iloc[0]
@@ -65,10 +23,18 @@ for task in top_models["task"].unique():
         plt.figure(figsize=(6, 5))
         ax = sns.heatmap(confmat, annot=True, fmt="d", cmap="Greys", cbar=False, annot_kws={"size": 40})
         
-        # Extract task name and weights
+        if task == "A":
+            coefs = [0.8, 1.3]
+        elif task == "B":
+            coefs = [0.65, 1.675, 1.15]
+        elif task == "C":
+            coefs = [5.1, 0.56]
         task_name, weights = model_id.split("_")
-        weight_parts = weights.split("-")
-        formatted_weights = "-".join(f"{int(w)/10:.1f}" for w in weight_parts)
+        weight_parts = [float(w) for w in weights.split("-")]
+        weighted_parts = [w * c for w, c in zip(weight_parts, coefs)]
+
+        formatted_weights = "-".join(f"{float(w)/10:.1f}" for w in weighted_parts)
+        
         title_line1 = f"Task {task_name} (weights: {formatted_weights})"
 
         title_line2 = (
@@ -82,7 +48,6 @@ for task in top_models["task"].unique():
         ax.set_xlabel("Predicted", fontsize=21)
         ax.set_ylabel("True", fontsize=21)
 
-        # Set proper tick labels based on task
         if task == "A":
             ax.set_xticklabels(["NOT", "HOF"], fontsize=21)
             ax.set_yticklabels(["NOT", "HOF"], fontsize=21)
